d72c82ace5d8a77ab5174b4782057ce0596c3cb4
SiriwardanaT <74727810+SiriwardanaT@users.noreply.github.com>
2021-02-27 19:24:07 +0530

    initial commint

---
+++ WebApp/src/forservlet/Text01Servlet.java
@@ -0,0 +1,30 @@
+package forservlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Text01Servlet")
+public class Text01Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public Text01Servlet() {
+       
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+	}
+
+}
